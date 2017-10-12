69b82e68961347a3b7295e1bb1a9ce59f62ff91f
Matt Butcher <matt.butcher@microsoft.com>
2017-10-12 15:50:45 -0600

    Update JS to send Slack message

--- brigade.js
+++ brigade.js
@@ -1,15 +1,29 @@
 const {events, Job} = require("brigadier")
 
-const moveAction = "action_move_card_from_list_to_list"
+const ACTION_MOVE = "action_move_card_from_list_to_list"
 
 events.on("trello", (e, p) => {
+  // Parse the JSON payload from Trello.
   var hook = JSON.parse(e.payload)
   var d = hook.action.display
+  var e = d.entities
 
-  if (d.translationKey = moveAction) {
-    console.log(d)
-    var e = d.entities
-    console.log(`Card ${e.card.text} moved from ${e.listBefore.text} to ${e.listAfter.text}`)
+  // Ignore other events. Just capture moves.
+  if (d.translationKey != ACTION_MOVE) {
+    return
   }
+
+  // Message to send to Slack
+  var m = `Card "${e.card.text}" moved from "${e.listBefore.text}" to "${e.listAfter.text}" <${hook.model.shortURL}>`)
+
+  // Slack job will send the message.
+  var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+  slack.env = {
+    SLACK_WEBHOOK: project.secrets.SLACK_WEBHOOK,
+    SLACK_USERNAME: "BrigadeBot",
+    SLACK_TITLE: `Update to card ${e.card.text}`,
+    SLACK_MESSAGE: m
+  }
+  slack.run()
 
 })
