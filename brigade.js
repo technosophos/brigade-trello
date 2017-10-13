a5306d2ea0d37f28677d05b014da65fdcfa9c327
Matt Butcher <matt.butcher@microsoft.com>
2017-10-12 19:42:50 -0600

    Fixed ping in Slack

--- brigade.js
+++ brigade.js
@@ -22,7 +22,7 @@ events.on("trello", (e, p) => {
   console.log(`--eval 'db.trello.insert(${e.payload})'`)
 
   // Message to send to Slack
-  var m = `From "${d.entities.listBefore.text}" to "${d.entities.listAfter.text}" <${hook.model.shortUrl}>`
+  var m = `From "${d.entities.listBefore.text}" to "${d.entities.listAfter.text}" <${hook.model.shortUrl}> <U0RMKK605>`
 
   // Slack job will send the message.
   var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
@@ -30,7 +30,7 @@ events.on("trello", (e, p) => {
   slack.env = {
     SLACK_WEBHOOK: p.secrets.SLACK_WEBHOOK,
     SLACK_USERNAME: "Trello",
-    SLACK_TITLE: `Moved "${d.entities.card.text}" for <@technosophos>`,
+    SLACK_TITLE: `Moved "${d.entities.card.text}"`,
     SLACK_MESSAGE: m,
     SLACK_ICON: "https://a.trellocdn.com/images/ios/0307bc39ec6c9ff499c80e18c767b8b1/apple-touch-icon-152x152-precomposed.png"
   }
