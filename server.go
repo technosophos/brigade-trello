d617eebeef8c43747d3eb234341b6a19a4f6d9bf
Matt Butcher <matt.butcher@microsoft.com>
2018-02-09 21:34:10 -0700

    feat(generic): add generic webhook

--- server.go
+++ server.go
@@ -24,19 +24,27 @@ func main() {
 	}
 	store = kube.New(client, "default") // FIXME: accept alt namespaces
 
-	router := gin.Default()
-
+	//router := gin.Default()
+	router := gin.New()
+	router.Use(gin.Recovery())
 	router.GET("/healthz", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "ok"}) })
 
 	// Trello uses GET/HEAD to test connection
-	router.GET("/trello/:project", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "ok"}) })
-	router.HEAD("/trello/:project", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "ok"}) })
-	router.POST("/trello/:project", trello)
+	trello := router.Group("/trello")
+	trello.Use(gin.Logger())
+	trello.GET("/:project", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "ok"}) })
+	trello.HEAD("/:project", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "ok"}) })
+	trello.POST("/:project", trelloFn)
+
+	// Generic is just a generic webhook handler
+	generic := router.Group("/generic")
+	generic.Use(gin.Logger())
+	generic.GET("/:project", genericFn)
 
 	router.Run()
 }
 
-func trello(c *gin.Context) {
+func trelloFn(c *gin.Context) {
 	pid := c.Param("project")
 
 	body, err := ioutil.ReadAll(c.Request.Body)
@@ -67,6 +75,50 @@ func trello(c *gin.Context) {
 		ProjectID: pid,
 		Type:      "trello",
 		Provider:  "trello",
+		Commit:    "master",
+		Payload:   body,
+		Script:    script,
+	}
+	if err := store.CreateBuild(build); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "Failed to invoke hook"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "OK"})
+}
+
+func genericFn(c *gin.Context) {
+	log.Printf("generic webhook from user-agent %q", c.Request.UserAgent())
+	pid := c.Param("project")
+
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "Malformed body"})
+		return
+	}
+	c.Request.Body.Close()
+
+	// Load project
+	proj, err := store.GetProject(pid)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"status": "Resource Not Found"})
+		return
+	}
+
+	// Get the brigade.js
+	// Right now, we skip this and let the github project handle it.
+	script, err := webhook.GetFileContents(proj, "master", "brigade.js")
+	if err != nil {
+		log.Printf("Error getting file: %s", err)
+		c.JSON(http.StatusNotFound, gin.H{"status": "Script Not Found"})
+		return
+	}
+
+	// Create the build
+	build := &brigade.Build{
+		ProjectID: pid,
+		Type:      "webhook",
+		Provider:  c.Request.UserAgent(),
 		Commit:    "master",
 		Payload:   body,
 		Script:    script,
